567e5313eb039458ac6a24648ef0d5400f676b82
Abhijeet Singh <abhi009215@gmail.com>
2021-12-17 17:12:03 +0530

    upadate

--- Host-HIV.sh
+++ Host-HIV.sh
@@ -6,11 +6,11 @@
 function functionu(){
 echo ""
 echo "Testing for Host Header injection vulnerability on $uo"
-responseA=$(curl -sk -D response $uo -H 'Host: bing.com' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0' -I)
+response=$(curl -sk -D response $uo -H 'Host: bing.com' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0' -I)
 response=$(curl -sk -o body.txt $uo -H 'Host: bing.com' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0')
 
 cat body.txt >> response
-rm -r ./body.txt
+rm -r ./body.txt 
 finding=$(cat response | grep bing.com)
 rm -r response
 if [ -z "$finding" ]; then
@@ -22,6 +22,7 @@ fi
 }
 
 function functionU(){
+echo ""
 while IFS= read -r line; do
 	uo=$line
 	functionu
@@ -37,22 +38,34 @@ while getopts u:U:hc options; do
 				u) uo=$OPTARG;;
 				U) Uo=$OPTARG;;
 				h) ho=$OPTARG;;
-				c) co=$OPTARG;;
-				p) po=$OPTARG;;
 				*) echo "Try using valid options. use -h to show help menu.";;
 
 		esac
 		if [[ $options = "u" ]]; then
 			functionu
 		elif [[ $options = "U" ]]; then
-			functionU < $2
+			echo "Checking if the supplied list of URLs are working fine."
+			sleep 3
+			cat ./$2 | httpx | tee $2.urls
+			echo ""
+			sleep 1
+			echo "Nice!! We have collected the working urls in $2.urls file."
+			echo ""
+			sleep 2
+			echo "Now checking for Host Header Injection vulnerability. The output will be saved to vulnerableUrls.txt"
+			sleep 3
+			functionU < $2.urls
 			if [ -s vulnerableUrls.txt ]; then
 			    # The file is not-empty.
+			     sleep 2
+				echo ""
 				echo "The vulnerable URLs has been saved to vulnerableUrls.txt"
 			else
 		        # The file is empty.
 		        echo "Sorry, no URL seems to be vulnerable. Better luck next time!!."
 			fi
+
+
 
 			echo "Finished the URLs"
 
@@ -64,7 +77,6 @@ while getopts u:U:hc options; do
 			echo ""
 			echo "eg: ./hhi.sh -u https://example.com"
 			echo "eg: ./hhi.sh -U ./path/URLlist.txt"
-
 		fi
 done
 #enhancement
